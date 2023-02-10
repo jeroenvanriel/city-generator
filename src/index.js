0ee78bc3185a7f951f1f88dfc4e28b398a769dba
Jeroen van Riel <jeroenvanriel@outlook.com>
2023-02-10 19:42:46 +0100

    THREE -> three

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-import * as THREE from 'three';
+import * as three from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import './style.css';
 import model from './model.glb';
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new three.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.outputEncoding = THREE.sRGBEncoding;
+renderer.outputEncoding = three.sRGBEncoding;
 document.body.appendChild(renderer.domElement);
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
+const camera = new three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
 const controls = new OrbitControls(camera, renderer.domElement);
 camera.position.set(10, 10, 10);
 controls.target.set(0, 8, 0);
 controls.update()
 
 
-const scene = new THREE.Scene();
+const scene = new three.Scene();
 
 const loader = new GLTFLoader();
 loader.load(model, function(gltf) {
@@ -25,10 +25,10 @@ loader.load(model, function(gltf) {
   console.error(error);
 });
 
-const light = new THREE.AmbientLight(0x404040);
+const light = new three.AmbientLight(0x404040);
 scene.add(light);
 
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+const directionalLight = new three.DirectionalLight(0xffffff, 0.5);
 scene.add(directionalLight);
 
 
