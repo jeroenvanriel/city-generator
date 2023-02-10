a33fe06f2b5e5df5c636c0b9ac3428c3c5b8cb21
Jeroen van Riel <jeroenvanriel@outlook.com>
2023-02-10 20:01:22 +0100

    simple random blocks

---
+++ src/blocks.js
@@ -0,0 +1,23 @@
+import * as three from 'three';
+
+export default function blocks(scene) {
+    const material = new three.MeshBasicMaterial( {color: 0xff000f} );
+    const geometry = new three.BoxGeometry( 10, 10, 12 );
+
+    geometry.computeBoundingBox();
+    const size = new three.Vector3();
+    geometry.boundingBox.getSize(size)
+
+    for (let i = 1; i < 10; i += 1) {
+        const cube = new three.Mesh( geometry, material );
+        // make sure that the bottom of the buildings is at zero by adding half the height
+        cube.position.add(new three.Vector3(getRandomInt(0, 100), size.y / 2, getRandomInt(0, 100)))
+        scene.add( cube );
+    }
+
+}
+
+/** Returns a random integer between min and max. */
+function getRandomInt (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import './style.css';
 import model from './model.glb';
 
+import blocks from './blocks';
+
 const renderer = new three.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.outputEncoding = three.sRGBEncoding;
@@ -11,7 +13,7 @@ document.body.appendChild(renderer.domElement);
 
 const camera = new three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
 const controls = new OrbitControls(camera, renderer.domElement);
-camera.position.set(10, 10, 10);
+camera.position.set(150, 50, 150);
 controls.target.set(0, 8, 0);
 controls.update()
 
@@ -30,6 +32,9 @@ scene.add(light);
 
 const directionalLight = new three.DirectionalLight(0xffffff, 0.5);
 scene.add(directionalLight);
+
+
+blocks(scene)
 
 
 function animate() {
