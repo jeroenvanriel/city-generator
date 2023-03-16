c26d41e9963dab951f19dd4df15c71bd8e3ef54e
Jeroen van Riel <jeroenvanriel@outlook.com>
2023-03-16 17:15:01 +0100

    cut the rowhouse segments at random

--- src/rowhouse.js
+++ src/rowhouse.js
@@ -1,7 +1,7 @@
 import * as three from 'three';
 import { RowhouseGeometry } from './rowhouseGeometry';
 import { RowhouseRoofGeometry } from './rowhouseRoofGeometry.js';
-import { polygonToMesh, offsetPolygon, toClipper, fromClipper, SCALE, extrudePolyline, asVector2List } from './utils';
+import { polygonToMesh, offsetPolygon, toClipper, fromClipper, SCALE, extrudePolyline, asVector2List, polygonToShape, randomUniform, getRandomInt } from './utils';
 
 import { red, blue } from './material.js';
 
@@ -15,9 +15,25 @@ export function buildRowHouses(scene, clipper, hole) {
   // draw sidewalks
   scene.add(polygonToMesh([hole, sidewalkInner], red));
 
-  const row = fromClipper(offsetPolygon(clipper, toClipper(hole), - (houseOffset) * SCALE));
+  // get row house segments (polylines)
+  const innerPolygon = fromClipper(offsetPolygon(clipper, toClipper(hole), - (houseOffset) * SCALE));
+  const segments = getSegments(innerPolygon);
+  const houseLines = cutSegments(segments)
 
+  for (const segment of houseLines) {
+    const basePolygon = fromClipper(extrudePolyline(clipper, toClipper(segment), houseDepth * SCALE));
+    basePolygon.push(basePolygon[0]); // close it
+
+    buildHouse(scene,
+      asVector2List(basePolygon),
+      asVector2List(segment)
+    )
+  }
+}
+
+function getSegments(row) {
   const THRESHOLD = 5;
+
   let segments = [];
   // current consecutive
   let currentSegment = [row[0]];
@@ -41,15 +57,52 @@ export function buildRowHouses(scene, clipper, hole) {
     segments.push(currentSegment)
   }
 
-  for (const segment of segments) {
-    const basePolygon = fromClipper(extrudePolyline(clipper, toClipper(segment), houseDepth * SCALE));
-    basePolygon.push(basePolygon[0]); // close it
+  return segments;
+}
 
-    buildHouse(scene,
-      asVector2List(basePolygon),
-      asVector2List(segment)
-    )
+function cutSegments(segments) {
+  const MAX_CUTS = 2;
+  const lines = [];
+
+  for (const segment of segments) {
+    let currentLine = [segment[0]];
+
+    for (let i = 1; i < segment.length; i++) {
+      const cutsCount = getRandomInt(0, MAX_CUTS);
+
+      if (cutsCount == 0) {
+        currentLine.push(segment[i]);
+        continue;
+      }
+
+      const line = new three.LineCurve(
+        new three.Vector2(segment[i-1][0], segment[i-1][1]),
+        new three.Vector2(segment[i][0], segment[i][1]),
+      );
+      const tangent = line.getTangent(0.5); // t does not matter for lines
+      // divisions = cuts + 1
+      const cuts = line.getSpacedPoints(cutsCount + 1).slice(1, cutsCount + 1);
+
+      for (const cut of cuts) {
+        // cut the line here
+        const p = cut.clone();
+        p.addScaledVector(tangent, -10);
+        currentLine.push([p.x, p.y]);
+
+        lines.push(currentLine);
+
+        currentLine = [[cut.x, cut.y]];
+      }
+
+      currentLine.push(segment[i]);
+    }
+
+    if (currentLine.length >= 2) {
+      lines.push(currentLine);
+    }
   }
+
+  return lines;
 }
 
 function buildHouse(scene, basePoints, houseMidline) {
