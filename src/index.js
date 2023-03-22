61c61665f8254e02a4464680fe279c032c053b31
Jeroen van Riel <jeroenvanriel@outlook.com>
2023-03-22 09:50:36 +0100

    setup basic postprocessing

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './style.css';
 import Controls from './controls';
 import { build } from './build';
 
+import { EffectComposer, RenderPass, EffectPass, SMAAEffect, DepthOfFieldEffect } from 'postprocessing';
+
 import * as clipperLib from 'js-angusj-clipper/web';
 
 async function mainAsync() {
@@ -12,9 +14,12 @@ const clipper = await clipperLib.loadNativeClipperLibInstanceAsync(
   clipperLib.NativeClipperLibRequestedFormat.WasmWithAsmJsFallback
 );
 
-const renderer = new three.WebGLRenderer();
+const renderer = new three.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.outputEncoding = three.sRGBEncoding;
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = three.PCFShadowMap; //THREE.BasicShadowMap | THREE.PCFShadowMap |  THREE.VSMShadowMap | THREE.PCFSoftShadowMap
+
 document.body.appendChild(renderer.domElement);
 
 window.addEventListener('resize', onWindowResize, false);
@@ -24,6 +29,7 @@ function onWindowResize(){
   camera.updateProjectionMatrix();
 
   renderer.setSize( window.innerWidth, window.innerHeight );
+  composer.setSize( window.innerWidth, window.innerHeight)
 }
 
 const camera = new three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
@@ -31,20 +37,28 @@ const controls = new Controls(camera, renderer.domElement);
 
 const scene = new three.Scene();
 
-const light = new three.AmbientLight(0x404040);
-scene.add(light);
-
-const directionalLight = new three.DirectionalLight(0xffffff, 1);
-directionalLight.position.set(1, 1, 1).normalize();
-scene.add(directionalLight);
-
 // main drawing
 build(scene, clipper)
 
+const composer = new EffectComposer(renderer);
+composer.addPass(new RenderPass(scene, camera));
+
+const depthOfFieldEffect = new DepthOfFieldEffect(camera, {
+  focalLength: 0.3,
+  bokehScale: 2.0,
+  width: window.innerWidth, height: window.innerHeight
+});
+
+composer.addPass(new EffectPass(
+  camera,
+  depthOfFieldEffect,
+  new SMAAEffect(),
+));
+
 function animate() {
   requestAnimationFrame(animate);
-  controls.update()
-  renderer.render(scene, camera);
+  controls.update();
+  composer.render();
 }
 animate();
 
