e2d2cf3d3e02134424ab56dcae288bc778757d42
Jeroen van Riel <jeroenvanriel@outlook.com>
2023-04-08 13:40:00 +0200

    bird rotation match path tangent

--- src/birds.js
+++ src/birds.js
@@ -12,16 +12,24 @@ export default class Birds {
     this.addBirds(scene, bounds);
   }
 
-  addBirds(scene, bounds) {
+  addBirds(scene, bounds, drawPath=false) {
     loadObjects(BIRDS).then(r => {
       const obj = r.bird.obj.scene.clone();
       scene.add(obj);
 
+      const points = this.createPath(bounds)
       this.birds.push({
         'obj': obj,
-        'path': this.createPath(bounds),
-        'i': 0, // counter
+        'path': points,
+        'i': 0, // waypoint counter
       });
+ 
+      if (drawPath) {
+        const geometry = new three.BufferGeometry().setFromPoints( points );
+        const material = new three.LineBasicMaterial( { color: 0xff0000 } );
+        const curveObject = new three.Line( geometry, material );
+        scene.add(curveObject)
+      }
     });
   }
 
@@ -40,16 +48,18 @@ export default class Birds {
     }
 
     // create a closed wavey loop
-    const curve = new three.CatmullRomCurve3(points, true);
-    return curve.getPoints(waypoints);
+    const curve = new three.CatmullRomCurve3(points, true, 'centripetal', 0.1);
+    return curve.getSpacedPoints(waypoints);
   }
 
   update() {
     for (const bird of this.birds) {
-      if (bird.i >= bird.path.length) {
-        bird.i = 0;
-      }
+      bird.i = bird.i % bird.path.length;
+      let next = (bird.i + 1) % bird.path.length;
+
       bird.obj.position.copy(bird.path[bird.i]);
+      bird.obj.lookAt(bird.path[next]);
+
       bird.i++;
     }
   }
