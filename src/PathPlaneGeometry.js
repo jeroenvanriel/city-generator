d3e73aa1d79e092cd3adb0849f91cc135deb9da5
Jeroen van Riel <jeroenvanriel@outlook.com>
2023-09-10 20:26:40 +0200

    tabs to spaces

--- src/PathPlaneGeometry.js
+++ src/PathPlaneGeometry.js
@@ -1,77 +1,77 @@
 import * as three from 'three';
 import { BufferGeometry, Float32BufferAttribute } from 'three';
-import { distance, asVector2 } from './utils';
+import { distance } from './utils';
 
 class PathPlaneGeometry extends BufferGeometry {
 
-	constructor( bottomPoints, topPoints ) {
-		super();
+    constructor( bottomPoints, topPoints ) {
+        super();
 
-		this.type = 'PathPlaneGeometry';
+        this.type = 'PathPlaneGeometry';
 
         this.bottomPoints = bottomPoints;
-		this.topPoints = topPoints;
+        this.topPoints = topPoints;
 
-		// buffer
-		const vertices = [];
-		const indices = [];
-		const uvs = [];
+        // buffer
+        const vertices = [];
+        const indices = [];
+        const uvs = [];
 
-		// create buffer data
-		generateBufferData();
+        // create buffer data
+        generateBufferData();
 
-		// build geometry
-		this.setIndex( indices );
-		this.setAttribute( 'position', new Float32BufferAttribute( vertices, 3 ) );
-		this.setAttribute( 'uv', new Float32BufferAttribute( uvs, 2));
-		this.computeVertexNormals();
+        // build geometry
+        this.setIndex( indices );
+        this.setAttribute( 'position', new Float32BufferAttribute( vertices, 3 ) );
+        this.setAttribute( 'uv', new Float32BufferAttribute( uvs, 2));
+        this.computeVertexNormals();
 
-		function generateBufferData() {
-			for (let i = 0; i < topPoints.length - 1; i++) {
-				const p = bottomPoints[i];
-				const q = bottomPoints[i+1];
-				const r = topPoints[i];
-				const s = topPoints[i+1];
+        function generateBufferData() {
+            for (let i = 0; i < topPoints.length - 1; i++) {
+                const p = bottomPoints[i];
+                const q = bottomPoints[i+1];
+                const r = topPoints[i];
+                const s = topPoints[i+1];
 
-				vertices.push( p.x, p.y, p.z );
-				vertices.push( r.x, r.y, r.z );
-				vertices.push( s.x, s.y, s.z );
-				vertices.push( q.x, q.y, q.z );
+                vertices.push( p.x, p.y, p.z );
+                vertices.push( r.x, r.y, r.z );
+                vertices.push( s.x, s.y, s.z );
+                vertices.push( q.x, q.y, q.z );
 
-				const a = 4 * i;
-				const b = 4 * i + 1;
-				const c = 4 * i + 2;
-				const d = 4 * i + 3;
+                const a = 4 * i;
+                const b = 4 * i + 1;
+                const c = 4 * i + 2;
+                const d = 4 * i + 3;
 
-				// faces
-				indices.push( a, b, d );
-				indices.push( b, c, d );
+                // faces
+                indices.push( a, b, d );
+                indices.push( b, c, d );
 
                 // uvs
-				const width = distance(p, q);
-				const height1 = distance(p, r);
-				const height2 = distance(q, s);
+                const width = distance(p, q);
+                const height1 = distance(p, r);
+                const height2 = distance(q, s);
 
-				const v = new three.Vector3().subVectors(p, q).normalize();
+                const v = new three.Vector3().subVectors(p, q).normalize();
 
-				const w1 = new three.Vector3().subVectors(r, p);
-				const w2 = new three.Vector3().subVectors(s, q);
+                const w1 = new three.Vector3().subVectors(r, p);
+                const w2 = new three.Vector3().subVectors(s, q);
 
-				const shear1 = v.dot(w1);
-				const shear2 = -v.dot(w2)
+                const shear1 = v.dot(w1);
+                const shear2 = -v.dot(w2)
 
-				uvs.push(shear1, 0);
-				uvs.push(0, height1);
-				uvs.push(shear1 + width + shear2, height2);
-				uvs.push(shear1 + width, 0);
+                uvs.push(shear1, 0);
+                uvs.push(0, height1);
+                uvs.push(shear1 + width + shear2, height2);
+                uvs.push(shear1 + width, 0);
             }
         }
-	}
+    }
 
-	clone() {
-		// pass parameters
-		return new this.constructor(this.bottomPoints, this.topPoints).copy(this);
-	}
+    clone() {
+        // pass parameters
+        return new this.constructor(this.bottomPoints, this.topPoints).copy(this);
+    }
 
 }
 
--- src/straightSkeletonRoofGeometry.js
+++ src/straightSkeletonRoofGeometry.js
@@ -4,53 +4,53 @@ import earcut from 'earcut';
 
 class StraightSkeletonRoofGeometry extends BufferGeometry {
 
-	constructor(polygon, height) {
-		super();
+    constructor(polygon, height) {
+        super();
 
-		this.type = 'StraightSkeletonRoofGeometry';
-		this.height = height;
+        this.type = 'StraightSkeletonRoofGeometry';
+        this.height = height;
 
-		// buffer
-		const vertices = [];
-		const indices = [];
-		//const uvs = [];
+        // buffer
+        const vertices = [];
+        const indices = [];
+        //const uvs = [];
 
         generateBufferData();
-		this.setIndex( indices );
-		this.setAttribute('position', new Float32BufferAttribute(vertices, 3 ));
+        this.setIndex( indices );
+        this.setAttribute('position', new Float32BufferAttribute(vertices, 3 ));
 
         // TODO: compute uvs
-		//this.setAttribute('uv', new Float32BufferAttribute(uvs, 2));
-		//this.computeVertexNormals();
+        //this.setAttribute('uv', new Float32BufferAttribute(uvs, 2));
+        //this.computeVertexNormals();
 
         function generateBufferData() {
-			// compute the straight skeleton
-			const result = SkeletonBuilder.buildFromPolygon([
-				polygon.map((point) => [point.x, point.y])
-			]);
+            // compute the straight skeleton
+            const result = SkeletonBuilder.buildFromPolygon([
+                polygon.map((point) => [point.x, point.y])
+            ]);
 
-			// triangulate computed polygons
-			let index_counter = 0;
-			for (const polygon of result.polygons) {
+            // triangulate computed polygons
+            let index_counter = 0;
+            for (const polygon of result.polygons) {
                 // use the original 3d points as vertices
-				let flat3 = polygon.map((point) => result.vertices[point]);
+                let flat3 = polygon.map((point) => result.vertices[point]);
                 flat3 = flat3.map(([x,y,z]) => [x,z,y]).flat();
-				vertices.push(...flat3);
+                vertices.push(...flat3);
 
                 // triangulate the flat polygons to obtain indices
-				const flat2 = polygon.map((point) => result.vertices[point].slice(0, 2)).flat();
-				let triangulated = earcut(flat2);
+                const flat2 = polygon.map((point) => result.vertices[point].slice(0, 2)).flat();
+                let triangulated = earcut(flat2);
                 triangulated.reverse();
-				indices.push(...triangulated.map((index) => index_counter + index))
+                indices.push(...triangulated.map((index) => index_counter + index))
 
-				index_counter += flat3.length / 3;
-			}
+                index_counter += flat3.length / 3;
+            }
         }
-	}
+    }
 
-	clone() {
-		return new this.constructor(this.midline, this.height).copy(this);
-	}
+    clone() {
+        return new this.constructor(this.midline, this.height).copy(this);
+    }
 
 }
 
